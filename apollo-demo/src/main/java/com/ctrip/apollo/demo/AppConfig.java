902161760834a6491ae28c95384de1b423465365
Jason Song <song_s@ctrip.com>
2016-03-14 14:50:25 +0800

    init apollo demo

---
+++ apollo-demo/src/main/java/com/ctrip/apollo/demo/AppConfig.java
@@ -0,0 +1,12 @@
+package com.ctrip.apollo.demo;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by jason on 3/8/16.
+ */
+@Configuration
+@ComponentScan(value = "com.ctrip.apollo.demo")
+public class AppConfig {
+}
