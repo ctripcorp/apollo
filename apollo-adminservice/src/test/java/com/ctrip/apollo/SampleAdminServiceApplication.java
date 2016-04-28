92e06baa9189f142bfd2a5e92fcb6d7ce4a475c7
lepdou <ledou.zhang@dianping.com>
2016-04-28 18:14:33 +0800

    fix

--- apollo-adminservice/src/test/java/com/ctrip/apollo/SampleAdminServiceApplication.java
+++ apollo-adminservice/src/test/java/com/ctrip/apollo/SampleAdminServiceApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class SampleAdminServiceApplication {
   public static void main(String[] args) {
     new SpringApplicationBuilder(SampleAdminServiceApplication.class).run(args);
