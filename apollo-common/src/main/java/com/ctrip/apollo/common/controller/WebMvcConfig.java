38243d7b0d9ba5dc1e84c98ad13839b61cdbff6e
Jason Song <nobodyiam@gmail.com>
2016-05-10 19:21:33 +0800

    use gson to improve performance

--- apollo-common/src/main/java/com/ctrip/apollo/common/controller/WebMvcConfig.java
+++ apollo-common/src/main/java/com/ctrip/apollo/common/controller/WebMvcConfig.java
@@ -1,14 +1,21 @@
 package com.ctrip.apollo.common.controller;
 
-import java.util.List;
+import com.google.common.collect.Lists;
 
+import org.springframework.boot.autoconfigure.web.HttpMessageConverters;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
 import org.springframework.http.MediaType;
+import org.springframework.http.converter.ByteArrayHttpMessageConverter;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.GsonHttpMessageConverter;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.util.List;
 
 @Configuration
 public class WebMvcConfig extends WebMvcConfigurerAdapter {
@@ -25,5 +32,17 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
   @Override
   public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
     configurer.ignoreAcceptHeader(true).defaultContentType(MediaType.APPLICATION_JSON);
+  }
+
+  @Bean
+  public HttpMessageConverters messageConverters() {
+    final List<HttpMessageConverter<?>> converters =
+        Lists.newArrayList(new ByteArrayHttpMessageConverter(), new GsonHttpMessageConverter());
+    return new HttpMessageConverters() {
+      @Override
+      public List<HttpMessageConverter<?>> getConverters() {
+        return converters;
+      }
+    };
   }
 }
