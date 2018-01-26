8583160cf4c433e0af9c9a0413a6faa728f33e2d
tanjian <tanjian20150101@163.com>
2018-01-26 09:49:33 +0800

    优化管理项目添加管理员，管理员登陆查看不了项目的问题。

--- apollo-portal/src/main/java/com/ctrip/framework/apollo/portal/repository/RoleRepository.java
+++ apollo-portal/src/main/java/com/ctrip/framework/apollo/portal/repository/RoleRepository.java
@@ -4,6 +4,8 @@ import com.ctrip.framework.apollo.portal.entity.po.Role;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
 
+import java.util.List;
+
 /**
  * @author Jason Song(song_s@ctrip.com)
  */
@@ -12,4 +14,10 @@ public interface RoleRepository extends PagingAndSortingRepository<Role, Long> {
    * find role by role name
    */
   Role findTopByRoleName(String roleName);
+
+
+  /**
+   * find role by roleName and roleIds
+   */
+  List<Role> findAllByIdInAndRoleNameStartingWith(List<Long> roleIds, String roleName);
 }
