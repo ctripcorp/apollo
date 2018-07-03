35c8dfa1d15d51f3e1293a3cfc17aca13857b44a
姜年珩.信息技术部 <jiangnianheng@aibank.com>
2018-07-04 01:33:45 +0800

    Print log by log level.

--- apollo-common/src/main/java/com/ctrip/framework/apollo/common/controller/GlobalDefaultExceptionHandler.java
+++ apollo-common/src/main/java/com/ctrip/framework/apollo/common/controller/GlobalDefaultExceptionHandler.java
@@ -8,6 +8,7 @@ import com.ctrip.framework.apollo.tracer.Tracer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.event.Level;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -43,48 +44,43 @@ public class GlobalDefaultExceptionHandler {
   //处理系统内置的Exception
   @ExceptionHandler(Throwable.class)
   public ResponseEntity<Map<String, Object>> exception(HttpServletRequest request, Throwable ex) {
-    return handleError(request, INTERNAL_SERVER_ERROR, ex, false);
+    return handleError(request, INTERNAL_SERVER_ERROR, ex);
   }
 
   @ExceptionHandler({HttpRequestMethodNotSupportedException.class, HttpMediaTypeException.class})
   public ResponseEntity<Map<String, Object>> badRequest(HttpServletRequest request,
                                                         ServletException ex) {
-    return handleError(request, BAD_REQUEST, ex, true);
+    return handleError(request, BAD_REQUEST, ex, Level.WARN);
   }
 
   @ExceptionHandler(HttpStatusCodeException.class)
   public ResponseEntity<Map<String, Object>> restTemplateException(HttpServletRequest request,
                                                                    HttpStatusCodeException ex) {
-    return handleError(request, ex.getStatusCode(), ex, false);
+    return handleError(request, ex.getStatusCode(), ex);
   }
 
   @ExceptionHandler(AccessDeniedException.class)
   public ResponseEntity<Map<String, Object>> accessDeny(HttpServletRequest request,
                                                         AccessDeniedException ex) {
-    return handleError(request, FORBIDDEN, ex, false);
+    return handleError(request, FORBIDDEN, ex);
   }
 
   //处理自定义Exception
   @ExceptionHandler({AbstractApolloHttpException.class})
   public ResponseEntity<Map<String, Object>> badRequest(HttpServletRequest request, AbstractApolloHttpException ex) {
-    return handleError(request, ex);
+    return handleError(request, ex.getHttpStatus(), ex, Level.ERROR);
   }
 
-
+  
   private ResponseEntity<Map<String, Object>> handleError(HttpServletRequest request,
-                                                          AbstractApolloHttpException ex) {
-    return handleError(request, ex.getHttpStatus(), ex, false);
+                                                          HttpStatus status, Throwable ex) {
+    return handleError(request, status, ex, Level.ERROR);
   }
 
-
   private ResponseEntity<Map<String, Object>> handleError(HttpServletRequest request,
-                                                          HttpStatus status, Throwable ex, boolean isWarn) {
+                                                          HttpStatus status, Throwable ex, Level logLevel) {
     String message = ex.getMessage();
-
-    if(isWarn)	logger.warn(message, ex);
-    else 		logger.error(message, ex);
-    Tracer.logError(ex);
-
+    printLog(message, ex, logLevel);
 
     Map<String, Object> errorAttributes = new HashMap<>();
     boolean errorHandled = false;
@@ -112,6 +108,29 @@ public class GlobalDefaultExceptionHandler {
     HttpHeaders headers = new HttpHeaders();
     headers.setContentType(APPLICATION_JSON);
     return new ResponseEntity<>(errorAttributes, headers, status);
+  }
+
+  //打印日志, 其中logLevel为日志级别: ERROR/WARN/DEBUG/INFO/TRACE
+  private void printLog(String message, Throwable ex, Level logLevel) {
+    switch (logLevel.toString()) {
+      case "ERROR":
+        logger.error(message, ex);
+        break;
+      case "WARN":
+        logger.warn(message, ex);
+        break;
+      case "DEBUG":
+        logger.debug(message, ex);
+        break;
+      case "INFO":
+        logger.info(message, ex);
+        break;
+      case "TRACE":
+        logger.trace(message, ex);
+        break;
+    }
+    
+    Tracer.logError(ex);
   }
 
 }
