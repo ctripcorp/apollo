82fa0730c283571bd95e4cde6b88e0152290af6e
birdie7761 <wu.diablo@gmail.com>
2018-10-30 08:38:41 +0800

    调整格式

--- apollo-configservice/src/main/java/com/ctrip/framework/apollo/configservice/ConfigServiceAutoConfiguration.java
+++ apollo-configservice/src/main/java/com/ctrip/framework/apollo/configservice/ConfigServiceAutoConfiguration.java
@@ -11,6 +11,7 @@ import com.ctrip.framework.apollo.configservice.service.ReleaseMessageServiceWit
 import com.ctrip.framework.apollo.configservice.service.config.ConfigService;
 import com.ctrip.framework.apollo.configservice.service.config.ConfigServiceWithCache;
 import com.ctrip.framework.apollo.configservice.service.config.DefaultConfigService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -22,57 +23,57 @@ import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 @Configuration
 public class ConfigServiceAutoConfiguration {
 
-  @Autowired
-  private BizConfig bizConfig;
-
-  @Bean
-  public GrayReleaseRulesHolder grayReleaseRulesHolder() {
-    return new GrayReleaseRulesHolder();
-  }
-
-  @Bean
-  public ConfigService configService() {
-    if (bizConfig.isConfigServiceCacheEnabled()) {
-      return new ConfigServiceWithCache();
-    }
-    return new DefaultConfigService();
-  }
-
-  @Bean
-  public static NoOpPasswordEncoder passwordEncoder() {
-    return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
-  }
-
-  @Configuration
-  static class MessageScannerConfiguration {
     @Autowired
-    private NotificationController notificationController;
-    @Autowired
-    private ConfigFileController configFileController;
-    @Autowired
-    private NotificationControllerV2 notificationControllerV2;
-    @Autowired
-    private GrayReleaseRulesHolder grayReleaseRulesHolder;
-    @Autowired
-    private ReleaseMessageServiceWithCache releaseMessageServiceWithCache;
-    @Autowired
-    private ConfigService configService;
+    private BizConfig bizConfig;
 
     @Bean
-    public ReleaseMessageScanner releaseMessageScanner() {
-      ReleaseMessageScanner releaseMessageScanner = new ReleaseMessageScanner();
-      //0. handle release message cache
-      releaseMessageScanner.addMessageListener(releaseMessageServiceWithCache);
-      //1. handle gray release rule
-      releaseMessageScanner.addMessageListener(grayReleaseRulesHolder);
-      //2. handle server cache
-      releaseMessageScanner.addMessageListener(configService);
-      releaseMessageScanner.addMessageListener(configFileController);
-      //3. notify clients
-      releaseMessageScanner.addMessageListener(notificationControllerV2);
-      releaseMessageScanner.addMessageListener(notificationController);
-      return releaseMessageScanner;
+    public GrayReleaseRulesHolder grayReleaseRulesHolder() {
+        return new GrayReleaseRulesHolder();
     }
-  }
+
+    @Bean
+    public ConfigService configService() {
+        if (bizConfig.isConfigServiceCacheEnabled()) {
+            return new ConfigServiceWithCache();
+        }
+        return new DefaultConfigService();
+    }
+
+    @Bean
+    public static NoOpPasswordEncoder passwordEncoder() {
+        return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
+    }
+
+    @Configuration
+    static class MessageScannerConfiguration {
+        @Autowired
+        private NotificationController notificationController;
+        @Autowired
+        private ConfigFileController configFileController;
+        @Autowired
+        private NotificationControllerV2 notificationControllerV2;
+        @Autowired
+        private GrayReleaseRulesHolder grayReleaseRulesHolder;
+        @Autowired
+        private ReleaseMessageServiceWithCache releaseMessageServiceWithCache;
+        @Autowired
+        private ConfigService configService;
+
+        @Bean
+        public ReleaseMessageScanner releaseMessageScanner() {
+            ReleaseMessageScanner releaseMessageScanner = new ReleaseMessageScanner();
+            //0. handle release message cache
+            releaseMessageScanner.addMessageListener(releaseMessageServiceWithCache);
+            //1. handle gray release rule
+            releaseMessageScanner.addMessageListener(grayReleaseRulesHolder);
+            //2. handle server cache
+            releaseMessageScanner.addMessageListener(configService);
+            releaseMessageScanner.addMessageListener(configFileController);
+            //3. notify clients
+            releaseMessageScanner.addMessageListener(notificationControllerV2);
+            releaseMessageScanner.addMessageListener(notificationController);
+            return releaseMessageScanner;
+        }
+    }
 
 }
