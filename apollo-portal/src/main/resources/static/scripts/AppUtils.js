fd017006daf6695b18580f282da861993689675e
SiouWang <w8774227@gmail.com>
2019-02-16 11:13:57 +0800

    remove debugger

--- apollo-portal/src/main/resources/static/scripts/AppUtils.js
+++ apollo-portal/src/main/resources/static/scripts/AppUtils.js
@@ -42,7 +42,6 @@ appUtil.service('AppUtil', ['toastr', '$window', '$q', function (toastr, $window
             if (!query) {
                 //如果不传这个参数或者false则返回到首页(参数出错)
                 if (!notJumpToHomePage) {
-                    debugger;
                     $window.location.href = '/index.html';
                 } else {
                     return {};
