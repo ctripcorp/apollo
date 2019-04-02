56919378154fc27c9fb62cbfa68ab2df0bcac003
kezhenxu94 <kezhenxu94@163.com>
2019-04-02 10:34:48 +0800

    Fix SSRF, resolve #2103

--- apollo-portal/src/main/resources/static/scripts/controller/SystemInfoController.js
+++ apollo-portal/src/main/resources/static/scripts/controller/SystemInfoController.js
@@ -28,13 +28,13 @@ function SystemInfoController($scope, toastr, AppUtil, AppService, ClusterServic
         });
     }
 
-    function check(host) {
-        SystemInfoService.check_health(host).then(function (result) {
+    function check(instanceId) {
+        SystemInfoService.check_health(instanceId).then(function (result) {
             var status = result.status.code;
-            if (status == 'UP') {
-                toastr.success(host + ' is healthy!');
+            if (status === 'UP') {
+                toastr.success(instanceId + ' is healthy!');
             } else {
-                toastr.error(host + ' is not healthy, please check ' + host + '/health for more information!');
+                toastr.error(instanceId + ' is not healthy, please check /health of ' + instanceId + 'for more information!');
             }
         }, function (result) {
             AppUtil.showErrorMsg(result);
