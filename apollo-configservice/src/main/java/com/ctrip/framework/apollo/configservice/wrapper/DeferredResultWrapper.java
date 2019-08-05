b97cd8a542586f79ddf3d9b1d626b4a992118bd9
nat-dmp <silverglittering@gmail.com>
2019-08-05 18:21:41 +0800

    com.ctrip.framework.foundation.internals.provider中timeout 配置化

--- apollo-configservice/src/main/java/com/ctrip/framework/apollo/configservice/wrapper/DeferredResultWrapper.java
+++ apollo-configservice/src/main/java/com/ctrip/framework/apollo/configservice/wrapper/DeferredResultWrapper.java
@@ -1,22 +1,31 @@
 package com.ctrip.framework.apollo.configservice.wrapper;
 
+import com.ctrip.framework.foundation.internals.Utils;
+import com.ctrip.framework.foundation.internals.provider.DefaultApplicationProvider;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 
 import com.ctrip.framework.apollo.core.dto.ApolloConfigNotification;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.context.request.async.DeferredResult;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Properties;
 
 /**
  * @author Jason Song(song_s@ctrip.com)
  */
 public class DeferredResultWrapper {
-  private static final long TIMEOUT = 60 * 1000;//60 seconds
+  //private static final long TIMEOUT = 60 * 1000;//60 seconds
+  private static final Logger logger = LoggerFactory.getLogger(DefaultApplicationProvider.class);
+  public static final String APP_PROPERTIES_CLASSPATH = "/META-INF/app.properties";
+  private Properties m_appProperties = new Properties();
+  private String m_timeout;
   private static final ResponseEntity<List<ApolloConfigNotification>>
       NOT_MODIFIED_RESPONSE_LIST = new ResponseEntity<>(HttpStatus.NOT_MODIFIED);
 
@@ -25,7 +34,7 @@ public class DeferredResultWrapper {
 
 
   public DeferredResultWrapper() {
-    result = new DeferredResult<>(TIMEOUT, NOT_MODIFIED_RESPONSE_LIST);
+    result = new DeferredResult<>(getTimeout(), NOT_MODIFIED_RESPONSE_LIST);
   }
 
   public void recordNamespaceNameNormalizedResult(String originalNamespaceName, String normalizedNamespaceName) {
@@ -64,5 +73,38 @@ public class DeferredResultWrapper {
 
   public DeferredResult<ResponseEntity<List<ApolloConfigNotification>>> getResult() {
     return result;
+  }
+
+  public long getTimeout() {
+    return Long.parseLong(m_timeout);
+  }
+
+  private void initTimeOut() {
+    // 1.Get timeout from System Property
+    m_timeout = System.getProperty("timeout");
+    if (!Utils.isBlank(m_timeout)) {
+      m_timeout = m_timeout.trim();
+      logger.info("timeout is set to {} by timeout property from System Property", m_timeout);
+      return;
+    }
+
+    // 2.Get timeout from OS environment variable
+    m_timeout = System.getenv("timeout");
+    if (!Utils.isBlank(m_timeout)) {
+      m_timeout = m_timeout.trim();
+      logger.info("timeout is set to {} by timeout property from OS environment variable", m_timeout);
+      return;
+    }
+
+    // 3.Get timeout from app.properties.
+    m_timeout = m_appProperties.getProperty("timeout");
+    if (!Utils.isBlank(m_timeout)) {
+      m_timeout = m_timeout.trim();
+      logger.info("timeout is set to {} by timeout property from app.properties.", m_timeout, APP_PROPERTIES_CLASSPATH);
+      return;
+    }
+
+    m_timeout = null;
+    logger.warn("timeout is not available from System Property and {}. It is set to null", APP_PROPERTIES_CLASSPATH);
   }
 }
