7211e89d3f5534cca884ac984dad5c105bfc5da7
Anilople <Anilople@outlook.com>
2020-05-17 22:26:34 +0800

    refactor(apollo-portal): export config file

--- apollo-portal/src/main/java/com/ctrip/framework/apollo/portal/controller/ConfigsExportController.java
+++ apollo-portal/src/main/java/com/ctrip/framework/apollo/portal/controller/ConfigsExportController.java
@@ -1,26 +1,26 @@
 package com.ctrip.framework.apollo.portal.controller;
 
 import com.ctrip.framework.apollo.common.exception.ServiceException;
-import com.ctrip.framework.apollo.core.ConfigConsts;
 import com.ctrip.framework.apollo.core.enums.ConfigFileFormat;
 import com.ctrip.framework.apollo.portal.entity.bo.NamespaceBO;
 import com.ctrip.framework.apollo.portal.environment.Env;
 import com.ctrip.framework.apollo.portal.service.NamespaceService;
-import com.ctrip.framework.apollo.portal.util.ConfigToFileUtils;
+import com.ctrip.framework.apollo.portal.util.NamespaceBOUtils;
 import com.google.common.base.Joiner;
 import com.google.common.base.Splitter;
 import java.util.List;
-import java.util.stream.Collectors;
 import javax.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * jian.tan
  */
 @RestController
+@RequestMapping("/apps")
 public class ConfigsExportController {
 
   private final NamespaceService namespaceService;
@@ -30,7 +30,7 @@ public class ConfigsExportController {
     this.namespaceService = namespaceService;
   }
 
-  @GetMapping("/apps/{appId}/envs/{env}/clusters/{clusterName}/namespaces/{namespaceName}/items/export")
+  @GetMapping("/{appId}/envs/{env}/clusters/{clusterName}/namespaces/{namespaceName}/items/export")
   public void exportItems(@PathVariable String appId, @PathVariable String env,
       @PathVariable String clusterName, @PathVariable String namespaceName,
       HttpServletResponse res) {
@@ -43,23 +43,11 @@ public class ConfigsExportController {
 
     //generate a file.
     res.setHeader("Content-Disposition", "attachment;filename=" + fileName);
-
-    List<String> fileItems = namespaceBO.getItems().stream().map(itemBO -> {
-      String key = itemBO.getItem().getKey();
-      String value = itemBO.getItem().getValue();
-      if (ConfigConsts.CONFIG_FILE_CONTENT_KEY.equals(key)) {
-        return value;
-      }
-
-      if ("".equals(key)) {
-        return Joiner.on("").join(itemBO.getItem().getKey(), itemBO.getItem().getValue());
-      }
-
-      return Joiner.on(" = ").join(itemBO.getItem().getKey(), itemBO.getItem().getValue());
-    }).collect(Collectors.toList());
-
+    // file content
+    final String configFileContent = NamespaceBOUtils.convert2configFileContent(namespaceBO);
     try {
-      ConfigToFileUtils.itemsToFile(res.getOutputStream(), fileItems);
+      // write content to net
+      res.getOutputStream().write(configFileContent.getBytes());
     } catch (Exception e) {
       throw new ServiceException("export items failed:{}", e);
     }
---
+++ apollo-portal/src/main/java/com/ctrip/framework/apollo/portal/util/NamespaceBOUtils.java
@@ -0,0 +1,40 @@
+package com.ctrip.framework.apollo.portal.util;
+
+import com.ctrip.framework.apollo.core.ConfigConsts;
+import com.ctrip.framework.apollo.portal.controller.ConfigsExportController;
+import com.ctrip.framework.apollo.portal.entity.bo.NamespaceBO;
+import com.google.common.base.Joiner;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * @author wxq
+ */
+public class NamespaceBOUtils {
+
+  /**
+   * copy from old {@link ConfigsExportController}.
+   */
+  public static String convert2configFileContent(NamespaceBO namespaceBO) {
+    List<String> fileItems = namespaceBO.getItems().stream().map(itemBO -> {
+      String key = itemBO.getItem().getKey();
+      String value = itemBO.getItem().getValue();
+
+      // special namespace format
+      if (ConfigConsts.CONFIG_FILE_CONTENT_KEY.equals(key)) {
+        return value;
+      }
+
+      if ("".equals(key)) {
+        return Joiner.on("").join(itemBO.getItem().getKey(), itemBO.getItem().getValue());
+      }
+
+      return Joiner.on(" = ").join(itemBO.getItem().getKey(), itemBO.getItem().getValue());
+    }).collect(Collectors.toList());
+
+    // merge every lines
+    String configFileContent = fileItems.stream().collect(Collectors.joining(System.lineSeparator()));
+    return configFileContent;
+  }
+
+}
