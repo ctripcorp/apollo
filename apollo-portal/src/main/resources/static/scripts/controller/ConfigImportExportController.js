88c83843238240b73e797228d9bc727ab066397e
Anilople <Anilople@outlook.com>
2020-05-30 15:36:59 +0800

    feat(apollo-portal): front end config import export

---
+++ apollo-portal/src/main/resources/static/scripts/controller/ConfigImportExportController.js
@@ -0,0 +1,7 @@
+config_import_export_module.controller('ConfigImportExportController',
+    ['$scope', '$window', '$translate', 'toastr', 'AppUtil', 'UserService', 'PermissionService',
+        ConfigImportExportController]);
+
+function ConfigImportExportController($scope, $window, $translate, toastr, AppUtil, UserService, PermissionService) {
+
+}
