c3cac5cac419b4c91b70b315a003c6e820ec366c
Anilople <Anilople@outlook.com>
2020-07-11 12:32:00 +0800

    feat(apollo-portal): use PropertiesUtil.toString(properties) to forbid value contain special char like '\n' due wrong format.

--- apollo-portal/src/main/java/com/ctrip/framework/apollo/portal/util/NamespaceBOUtils.java
+++ apollo-portal/src/main/java/com/ctrip/framework/apollo/portal/util/NamespaceBOUtils.java
@@ -1,11 +1,13 @@
 package com.ctrip.framework.apollo.portal.util;
 
 import com.ctrip.framework.apollo.core.ConfigConsts;
+import com.ctrip.framework.apollo.core.utils.PropertiesUtil;
 import com.ctrip.framework.apollo.portal.controller.ConfigsExportController;
+import com.ctrip.framework.apollo.portal.entity.bo.ItemBO;
 import com.ctrip.framework.apollo.portal.entity.bo.NamespaceBO;
-import com.google.common.base.Joiner;
+import java.io.IOException;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Properties;
 
 /**
  * @author wxq
@@ -16,26 +18,36 @@ public class NamespaceBOUtils {
    * copy from old {@link ConfigsExportController}.
    * convert {@link NamespaceBO} to a file content.
    * @return content of config file
+   * @throws IllegalStateException if convert properties to string fail
    */
   public static String convert2configFileContent(NamespaceBO namespaceBO) {
-    List<String> fileItems = namespaceBO.getItems().stream().map(itemBO -> {
+    List<ItemBO> itemBOS = namespaceBO.getItems();
+    // early return if it is not a properties format namespace
+    for (ItemBO itemBO : itemBOS) {
+      String key = itemBO.getItem().getKey();
+      // special namespace format(not properties)
+      if (ConfigConsts.CONFIG_FILE_CONTENT_KEY.equals(key)) {
+        return itemBO.getItem().getValue();
+      }
+    }
+
+    // it must be a properties format namespace
+    // save the kev value pair
+    Properties properties = new Properties();
+    for (ItemBO itemBO : itemBOS) {
       String key = itemBO.getItem().getKey();
       String value = itemBO.getItem().getValue();
+      // ignore comment, so the comment will lack
+      properties.put(key, value);
+    }
 
-      // special namespace format
-      if (ConfigConsts.CONFIG_FILE_CONTENT_KEY.equals(key)) {
-        return value;
-      }
-
-      if ("".equals(key)) {
-        return Joiner.on("").join(itemBO.getItem().getKey(), itemBO.getItem().getValue());
-      }
-
-      return Joiner.on(" = ").join(itemBO.getItem().getKey(), itemBO.getItem().getValue());
-    }).collect(Collectors.toList());
-
-    // merge every lines
-    String configFileContent = fileItems.stream().collect(Collectors.joining(System.lineSeparator()));
+    // use a special method convert properties to string
+    final String configFileContent;
+    try {
+      configFileContent = PropertiesUtil.toString(properties);
+    } catch (IOException e) {
+      throw new IllegalStateException("convert properties to string fail.", e);
+    }
     return configFileContent;
   }
 
