a17ee12fb8b44cd586ede782b1e21b465685f8b5
Anilople <anilople@outlook.com>
2020-07-12 15:30:46 +0800

    feat(apollo-portal): predicate namespace's format

--- apollo-portal/src/main/java/com/ctrip/framework/apollo/portal/util/NamespaceBOUtils.java
+++ apollo-portal/src/main/java/com/ctrip/framework/apollo/portal/util/NamespaceBOUtils.java
@@ -1,6 +1,7 @@
 package com.ctrip.framework.apollo.portal.util;
 
 import com.ctrip.framework.apollo.core.ConfigConsts;
+import com.ctrip.framework.apollo.core.enums.ConfigFileFormat;
 import com.ctrip.framework.apollo.core.utils.PropertiesUtil;
 import com.ctrip.framework.apollo.portal.controller.ConfigsExportController;
 import com.ctrip.framework.apollo.portal.entity.bo.ItemBO;
@@ -15,14 +16,13 @@ import java.util.Properties;
 public class NamespaceBOUtils {
 
   /**
-   * copy from old {@link ConfigsExportController}.
-   * convert {@link NamespaceBO} to a file content.
-   * @return content of config file
-   * @throws IllegalStateException if convert properties to string fail
+   * namespace must not be {@link ConfigFileFormat#Properties}.
+   * the content of namespace in item's value which item's key is {@link ConfigConsts#CONFIG_FILE_CONTENT_KEY}.
+   * @param namespaceBO namespace
+   * @return content of non-properties's namespace
    */
-  public static String convert2configFileContent(NamespaceBO namespaceBO) {
+  static String convertNonProperties2configFileContent(NamespaceBO namespaceBO) {
     List<ItemBO> itemBOS = namespaceBO.getItems();
-    // early return if it is not a properties format namespace
     for (ItemBO itemBO : itemBOS) {
       String key = itemBO.getItem().getKey();
       // special namespace format(not properties)
@@ -30,8 +30,26 @@ public class NamespaceBOUtils {
         return itemBO.getItem().getValue();
       }
     }
+    // If there is no items?
+    // return empty string ""
+    return "";
+  }
+
+  /**
+   * copy from old {@link ConfigsExportController}.
+   * convert {@link NamespaceBO} to a file content.
+   * @return content of config file
+   * @throws IllegalStateException if convert properties to string fail
+   */
+  public static String convert2configFileContent(NamespaceBO namespaceBO) {
+    // early return if it is not a properties format namespace
+    if (!ConfigFileFormat.Properties.equals(ConfigFileFormat.fromString(namespaceBO.getFormat()))) {
+      // it is not a properties namespace
+      return convertNonProperties2configFileContent(namespaceBO);
+    }
 
     // it must be a properties format namespace
+    List<ItemBO> itemBOS = namespaceBO.getItems();
     // save the kev value pair
     Properties properties = new Properties();
     for (ItemBO itemBO : itemBOS) {
