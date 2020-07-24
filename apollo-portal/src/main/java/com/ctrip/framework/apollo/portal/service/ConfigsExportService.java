45225b27e0c98518b160b80e89eff9f5a5c11b7d
Anilople <anilople@outlook.com>
2020-07-24 23:08:20 +0800

    style(apollo-portal): format code

--- apollo-portal/src/main/java/com/ctrip/framework/apollo/portal/service/ConfigsExportService.java
+++ apollo-portal/src/main/java/com/ctrip/framework/apollo/portal/service/ConfigsExportService.java
@@ -30,7 +30,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class ConfigsExportService {
 
-  private final static Logger logger = LoggerFactory.getLogger(ConfigsExportService.class);
+  private static final Logger logger = LoggerFactory.getLogger(ConfigsExportService.class);
 
   private final AppService appService;
 
@@ -56,44 +56,45 @@ public class ConfigsExportService {
   }
 
   /**
-   * write multiple namespace to a zip.
-   * use {@link Stream#reduce(Object, BiFunction, BinaryOperator)} to forbid concurrent write.
+   * write multiple namespace to a zip. use {@link Stream#reduce(Object, BiFunction,
+   * BinaryOperator)} to forbid concurrent write.
+   *
    * @param configBOStream namespace's stream
    * @param outputStream receive zip file output stream
-   * @throws IOException
+   * @throws IOException if happen write problem
    */
-  private static void writeAsZipOutputStream(Stream<ConfigBO> configBOStream, OutputStream outputStream)
-      throws IOException {
+  private static void writeAsZipOutputStream(
+      Stream<ConfigBO> configBOStream, OutputStream outputStream) throws IOException {
     try (final ZipOutputStream zipOutputStream = new ZipOutputStream(outputStream)) {
-      final Consumer<ConfigBO> configBOConsumer = configBO -> {
-        try {
-          // TODO, Stream.reduce will cause some problems. Is There other way to speed up the downloading?
-          synchronized (zipOutputStream) {
-            write2ZipOutputStream(zipOutputStream, configBO);
-          }
-        } catch (IOException e) {
-          logger.error("Write error. {}", configBO);
-          throw new IllegalStateException(e);
-        }
-      };
+      final Consumer<ConfigBO> configBOConsumer =
+          configBO -> {
+            try {
+              // TODO, Stream.reduce will cause some problems. Is There other way to speed up the
+              // downloading?
+              synchronized (zipOutputStream) {
+                write2ZipOutputStream(zipOutputStream, configBO);
+              }
+            } catch (IOException e) {
+              logger.error("Write error. {}", configBO);
+              throw new IllegalStateException(e);
+            }
+          };
       configBOStream.forEach(configBOConsumer);
     }
   }
 
   /**
-   * write {@link ConfigBO} as file to {@link ZipOutputStream}.
-   * Watch out the concurrent problem!
-   * zip output stream is same like cannot write concurrently!
-   * the name of file is determined by {@link ConfigFileUtils#toFilename(String, String, String, ConfigFileFormat)}.
-   * the path of file is determined by {@link ConfigFileUtils#toFilePath(String, String, Env, String)}.
+   * write {@link ConfigBO} as file to {@link ZipOutputStream}. Watch out the concurrent problem!
+   * zip output stream is same like cannot write concurrently! the name of file is determined by
+   * {@link ConfigFileUtils#toFilename(String, String, String, ConfigFileFormat)}. the path of file
+   * is determined by {@link ConfigFileUtils#toFilePath(String, String, Env, String)}.
+   *
    * @param zipOutputStream zip file output stream
    * @param configBO a namespace represent
    * @return zip file output stream same as parameter zipOutputStream
    */
   private static ZipOutputStream write2ZipOutputStream(
-      final ZipOutputStream zipOutputStream,
-      final ConfigBO configBO
-  ) throws IOException {
+      final ZipOutputStream zipOutputStream, final ConfigBO configBO) throws IOException {
     final Env env = configBO.getEnv();
     final String ownerName = configBO.getOwnerName();
     final String appId = configBO.getAppId();
@@ -101,7 +102,8 @@ public class ConfigsExportService {
     final String namespace = configBO.getNamespace();
     final String configFileContent = configBO.getConfigFileContent();
     final ConfigFileFormat configFileFormat = configBO.getFormat();
-    final String configFilename = ConfigFileUtils.toFilename(appId, clusterName, namespace, configFileFormat);
+    final String configFilename =
+        ConfigFileUtils.toFilename(appId, clusterName, namespace, configFileFormat);
     final String filePath = ConfigFileUtils.toFilePath(ownerName, appId, env, configFilename);
     final ZipEntry zipEntry = new ZipEntry(filePath);
     try {
@@ -115,69 +117,58 @@ public class ConfigsExportService {
     return zipOutputStream;
   }
 
-  /**
-   * @return the namespaces current user exists
-   */
+  /** @return the namespaces current user exists */
   private Stream<ConfigBO> makeStreamBy(
-      final Env env,
-      final String ownerName,
-      final String appId,
-      final String clusterName
-  ) {
-    final List<NamespaceBO> namespaceBOS = namespaceService.findNamespaceBOs(appId, env, clusterName);
-    final Function<NamespaceBO, ConfigBO> function = namespaceBO -> new ConfigBO(env, ownerName, appId, clusterName, namespaceBO);
-    return namespaceBOS.parallelStream()
-        .map(function);
+      final Env env, final String ownerName, final String appId, final String clusterName) {
+    final List<NamespaceBO> namespaceBOS =
+        namespaceService.findNamespaceBOs(appId, env, clusterName);
+    final Function<NamespaceBO, ConfigBO> function =
+        namespaceBO -> new ConfigBO(env, ownerName, appId, clusterName, namespaceBO);
+    return namespaceBOS.parallelStream().map(function);
   }
 
-  private Stream<ConfigBO> makeStreamBy(
-      final Env env,
-      final String ownerName,
-      final String appId
-  ) {
+  private Stream<ConfigBO> makeStreamBy(final Env env, final String ownerName, final String appId) {
     final List<ClusterDTO> clusterDTOS = clusterService.findClusters(env, appId);
-    final Function<ClusterDTO, Stream<ConfigBO>> function = clusterDTO -> this.makeStreamBy(env, ownerName, appId, clusterDTO.getName());
-    return clusterDTOS.parallelStream()
-        .flatMap(function);
+    final Function<ClusterDTO, Stream<ConfigBO>> function =
+        clusterDTO -> this.makeStreamBy(env, ownerName, appId, clusterDTO.getName());
+    return clusterDTOS.parallelStream().flatMap(function);
   }
 
-  private Stream<ConfigBO> makeStreamBy(
-      final Env env, final List<App> apps
-  ) {
-    final Function<App, Stream<ConfigBO>> function = app -> this.makeStreamBy(env, app.getOwnerName(), app.getAppId());
+  private Stream<ConfigBO> makeStreamBy(final Env env, final List<App> apps) {
+    final Function<App, Stream<ConfigBO>> function =
+        app -> this.makeStreamBy(env, app.getOwnerName(), app.getAppId());
 
-    return apps.parallelStream()
-        .flatMap(function);
+    return apps.parallelStream().flatMap(function);
   }
 
-  private Stream<ConfigBO> makeStreamBy(
-      final Collection<Env> envs
-  ) {
+  private Stream<ConfigBO> makeStreamBy(final Collection<Env> envs) {
     // get all apps
     final List<App> apps = appService.findAll();
 
     // permission check
-    final Predicate<App> isAppAdmin = app -> {
-      try{
-        return permissionValidator.isAppAdmin(app.getAppId());
-      } catch (Exception e) {
-        logger.error("app = {}", app);
-        logger.error(app.getAppId());
-      }
-      return false;
-    };
+    final Predicate<App> isAppAdmin =
+        app -> {
+          try {
+            return permissionValidator.isAppAdmin(app.getAppId());
+          } catch (Exception e) {
+            logger.error("app = {}", app);
+            logger.error(app.getAppId());
+          }
+          return false;
+        };
 
     // app admin permission filter
-    final List<App> appsExistPermission = apps.stream().filter(isAppAdmin).collect(Collectors.toList());
-    return envs.parallelStream()
-        .flatMap(env -> this.makeStreamBy(env, appsExistPermission));
+    final List<App> appsExistPermission =
+        apps.stream().filter(isAppAdmin).collect(Collectors.toList());
+    return envs.parallelStream().flatMap(env -> this.makeStreamBy(env, appsExistPermission));
   }
 
   /**
-   * Export all projects which current user own them.
-   * Permission check by {@link PermissionValidator#isAppAdmin(java.lang.String)}
+   * Export all projects which current user own them. Permission check by {@link
+   * PermissionValidator#isAppAdmin(java.lang.String)}
+   *
    * @param outputStream network file download stream to user
-   * @throws IOException
+   * @throws IOException if happen write problem
    */
   public void exportAllTo(OutputStream outputStream) throws IOException {
     final List<Env> activeEnvs = portalSettings.getActiveEnvs();
