cc1bbf7de43709bedc3f7f7c41ce8ae82b6f828e
anilople <Anilople@outlook.com>
2020-11-06 21:43:35 +0800

    test(apollo-client): '@EnableApolloConfig' resolving testing

--- apollo-client/src/test/java/com/ctrip/framework/apollo/spring/annotation/EnableApolloConfigTest.java
+++ apollo-client/src/test/java/com/ctrip/framework/apollo/spring/annotation/EnableApolloConfigTest.java
@@ -15,16 +15,42 @@ public class EnableApolloConfigTest extends AbstractSpringIntegrationTest {
   public void testResolveExpressionSimple() {
     mockConfig("application", mock(Config.class));
     mockConfig("xxx", mock(Config.class));
-    new AnnotationConfigApplicationContext(TestResolveExpressionSimpleConfiguration.class);
+    new AnnotationConfigApplicationContext(
+        TestResolveExpressionWithDefaultValueConfiguration.class);
   }
 
-  // TODO, cannot resolve "${simple.namespace:xxx}" in @EnableApolloConfig
-  // because have no embeddedValueResolvers in ConfigurableBeanFactory when use ImportBeanDefinitionRegistrar
+  @Test
+  public void testResolveExpressionFromSystemProperty() {
+    mockConfig("application", mock(Config.class));
+
+    final String resolvedNamespaceName = "yyy";
+    System.setProperty("from.system.property", resolvedNamespaceName);
+    mockConfig(resolvedNamespaceName, mock(Config.class));
+    new AnnotationConfigApplicationContext(
+        TestResolveExpressionFromSystemPropertyConfiguration.class);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testUnresolvable() {
+    new AnnotationConfigApplicationContext(TestUnresolvableConfiguration.class);
+  }
+
   @Configuration
   @EnableApolloConfig(value = {ConfigConsts.NAMESPACE_APPLICATION, "${simple.namespace:xxx}"})
-  static class TestResolveExpressionSimpleConfiguration {
+  static class TestResolveExpressionWithDefaultValueConfiguration {
 
   }
 
+  @Configuration
+  @EnableApolloConfig(value = {ConfigConsts.NAMESPACE_APPLICATION, "${from.system.property}"})
+  static class TestResolveExpressionFromSystemPropertyConfiguration {
+
+  }
+
+  @Configuration
+  @EnableApolloConfig(value = "${unresolvable.property}")
+  static class TestUnresolvableConfiguration {
+
+  }
 
 }
