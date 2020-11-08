4c0e7ded17ae3a475f6073e7cacd3fe0cf0e596a
anilople <Anilople@outlook.com>
2020-11-08 15:19:40 +0800

    docs(apollo-client): '@EnableApolloConfig' usage.

--- apollo-client/src/main/java/com/ctrip/framework/apollo/spring/annotation/EnableApolloConfig.java
+++ apollo-client/src/main/java/com/ctrip/framework/apollo/spring/annotation/EnableApolloConfig.java
@@ -14,10 +14,21 @@ import com.ctrip.framework.apollo.core.ConfigConsts;
 /**
  * Use this annotation to register Apollo property sources when using Java Config.
  *
- * <p>Configuration example:</p>
+ * <p>Configuration example with multiple namespaces:</p>
  * <pre class="code">
  * &#064;Configuration
  * &#064;EnableApolloConfig({"someNamespace","anotherNamespace"})
+ * public class AppConfig {
+ *
+ * }
+ * </pre>
+ *
+ * <p>Configuration example with placeholder:</p>
+ * <pre class="code">
+ * // The namespace could also be specified as a placeholder, e.g. ${redis.namespace:xxx},
+ * // which will use the value of the key "redis.namespace" or "xxx" if this key is not configured.
+ * &#064;Configuration
+ * &#064;EnableApolloConfig({"${redis.namespace:xxx}"})
  * public class AppConfig {
  *
  * }
