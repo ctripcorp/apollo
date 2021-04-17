78da8f13952ee8d5498c2d74a7b371b1179abae0
vdisk <vdisk@foxmail.com>
2021-04-17 15:41:42 +0800

    revert NamespaceBOUtils

--- apollo-portal/src/main/java/com/ctrip/framework/apollo/portal/util/NamespaceBOUtils.java
+++ apollo-portal/src/main/java/com/ctrip/framework/apollo/portal/util/NamespaceBOUtils.java
@@ -1,6 +1,5 @@
 package com.ctrip.framework.apollo.portal.util;
 
-import com.ctrip.framework.apollo.common.utils.PreferredUsernameUtil;
 import com.ctrip.framework.apollo.core.ConfigConsts;
 import com.ctrip.framework.apollo.core.enums.ConfigFileFormat;
 import com.ctrip.framework.apollo.core.utils.PropertiesUtil;
@@ -8,17 +7,8 @@ import com.ctrip.framework.apollo.portal.controller.ConfigsExportController;
 import com.ctrip.framework.apollo.portal.entity.bo.ItemBO;
 import com.ctrip.framework.apollo.portal.entity.bo.NamespaceBO;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
 import java.util.Properties;
-import java.util.Set;
-import java.util.function.Function;
-import java.util.stream.Collectors;
-import org.springframework.util.CollectionUtils;
 
 /**
  * @author wxq
@@ -79,94 +69,4 @@ public class NamespaceBOUtils {
     return configFileContent;
   }
 
-  /**
-   * enrich the preferred username for the namespace list
-   *
-   * @param namespaceList namespace list with operator id
-   * @param repository    preferred username repository (operatorIdList -> preferredUsernameMap)
-   */
-  public static void enrichPreferredUserName(List<NamespaceBO> namespaceList,
-      Function<List<String>, Map<String, String>> repository) {
-    if (CollectionUtils.isEmpty(namespaceList)) {
-      return;
-    }
-    Set<String> operatorIdSet = new HashSet<>();
-    for (NamespaceBO namespace : namespaceList) {
-      operatorIdSet.addAll(NamespaceBOUtils.extractOperatorId(namespace));
-    }
-    if (CollectionUtils.isEmpty(operatorIdSet)) {
-      return;
-    }
-    // userId - preferredUsername
-    Map<String, String> preferredUsernameMap = repository.apply(new ArrayList<>(operatorIdSet));
-    if (CollectionUtils.isEmpty(preferredUsernameMap)) {
-      return;
-    }
-    for (NamespaceBO namespace : namespaceList) {
-      NamespaceBOUtils.setPreferredUsername(namespace, preferredUsernameMap);
-    }
-  }
-
-  /**
-   * enrich the preferred username for the namespace
-   *
-   * @param namespace  namespace with operator id
-   * @param repository preferred username repository (operatorIdList -> preferredUsernameMap)
-   */
-  public static void enrichPreferredUserName(NamespaceBO namespace,
-      Function<List<String>, Map<String, String>> repository) {
-    if (namespace == null) {
-      return;
-    }
-    Set<String> operatorIdSet = NamespaceBOUtils.extractOperatorId(namespace);
-    if (CollectionUtils.isEmpty(operatorIdSet)) {
-      return;
-    }
-    // userId - preferredUsername
-    Map<String, String> preferredUsernameMap = repository.apply(new ArrayList<>(operatorIdSet));
-    if (CollectionUtils.isEmpty(preferredUsernameMap)) {
-      return;
-    }
-    NamespaceBOUtils.setPreferredUsername(namespace, preferredUsernameMap);
-  }
-
-  /**
-   * extract operator id from the namespace
-   *
-   * @param namespace namespace with operator id
-   * @return operator id set
-   */
-  public static Set<String> extractOperatorId(NamespaceBO namespace) {
-    if (namespace == null) {
-      return Collections.emptySet();
-    }
-    List<ItemBO> items = namespace.getItems();
-    if (CollectionUtils.isEmpty(items)) {
-      return Collections.emptySet();
-    }
-    return items.stream()
-        .map(ItemBO::getItem)
-        .map(PreferredUsernameUtil::extractOperatorId)
-        .flatMap(Collection::stream)
-        .collect(Collectors.toSet());
-  }
-
-  /**
-   * set the preferred username
-   *
-   * @param namespace            namespace with operator id
-   * @param preferredUsernameMap (userId - preferredUsername) prepared preferred username map
-   */
-  public static void setPreferredUsername(NamespaceBO namespace,
-      Map<String, String> preferredUsernameMap) {
-    if (namespace == null) {
-      return;
-    }
-    List<ItemBO> items = namespace.getItems();
-    if (CollectionUtils.isEmpty(items)) {
-      return;
-    }
-    items.forEach(itemBO -> PreferredUsernameUtil
-        .setPreferredUsername(itemBO.getItem(), preferredUsernameMap));
-  }
 }
