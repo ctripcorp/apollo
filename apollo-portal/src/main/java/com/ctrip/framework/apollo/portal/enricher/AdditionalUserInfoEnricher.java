87b083398b49d5dbd31907feb5cec82ff9ca886f
vdisk <vdisk@foxmail.com>
2021-05-22 10:10:47 +0800

    cleanup code

--- apollo-portal/src/main/java/com/ctrip/framework/apollo/portal/enricher/AdditionalUserInfoEnricher.java
+++ apollo-portal/src/main/java/com/ctrip/framework/apollo/portal/enricher/AdditionalUserInfoEnricher.java
@@ -1,11 +1,8 @@
 package com.ctrip.framework.apollo.portal.enricher;
 
-import com.ctrip.framework.apollo.common.dto.BaseDTO;
 import com.ctrip.framework.apollo.portal.enricher.adapter.UserInfoEnrichedAdapter;
 import com.ctrip.framework.apollo.portal.entity.bo.UserInfo;
-import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 /**
  * @author vdisk <vdisk@foxmail.com>
@@ -15,7 +12,7 @@ public interface AdditionalUserInfoEnricher {
   /**
    * enrich an additional user info for the dto list
    *
-   * @param adapter         enrich adapter
+   * @param adapter     enrich adapter
    * @param userInfoMap userInfo map
    */
   void enrichAdditionalUserInfo(UserInfoEnrichedAdapter adapter, Map<String, UserInfo> userInfoMap);
